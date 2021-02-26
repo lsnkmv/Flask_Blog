d1fcde7076cfdb2b1657a31368d7c1657b871c49
lsnkmv <63501096+lsnkmv@users.noreply.github.com>
2021-02-26 09:04:38 +0000

    Add files via upload

---
+++ flask_blog.py
@@ -0,0 +1,8 @@
+from app import app, db
+from app.models import User, Post
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+# TO DO: 1.fill all the tenplates with html code
+# 2.look up for errors due to refractoring pages
