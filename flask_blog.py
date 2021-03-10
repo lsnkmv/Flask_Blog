5e18ad9a019d3bb0c69eb75722b546816d66abe1
Leisan <chaylk@mail.ru>
2021-03-10 15:38:51 +0300

    Added new features

--- flask_blog.py
+++ flask_blog.py
@@ -3,6 +3,4 @@ from app.models import User, Post
 
 
 if __name__=='__main__':
-    app.run(debug=True)
-# TO DO: 1.fill all the tenplates with html code
-# 2.look up for errors due to refractoring pages
+    app.run(debug=False)
