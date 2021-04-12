027943c31157ed96af57649200eccd411b4a6964
Leisan <chaylk@mail.ru>
2021-04-12 22:35:47 +0300

    little update: working with localization (timezones)

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import os
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -21,6 +22,7 @@ mail = Mail(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
